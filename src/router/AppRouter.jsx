50c772a895512fde99cc94a67d0726be1601a589
hugorebel1998 <hugorebel1998@gmail.com>
2024-05-09 01:00:56 -0600

    Route: Protecciones de rutas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom"
 import { Login } from "../auth"
 import { HeroeRoutes } from "../heroes/routes/HeroeRoutes"
+import { PrivateRoute } from "./PrivateRoute"
+import { PublicRoute } from "./PublicRoute"
 
 export const AppRouter = () => {
     return (
@@ -8,9 +10,22 @@ export const AppRouter = () => {
 
             <Routes>
 
-                <Route path="login" element={<Login />} />
+                <Route path="login" element={
+                    <PublicRoute>
+                        <Login />
+                    </PublicRoute>
+                }
+                />
 
-                <Route path="/*" element={<HeroeRoutes />} />
+                {/* <Route path="login" element={<Login />} /> */}
+
+                <Route path="/*" element={
+                    <PrivateRoute>
+                        <HeroeRoutes />
+                    </PrivateRoute>
+                } />
+
+                {/* <Route path="/*" element={<HeroeRoutes />} /> */}
 
             </Routes>
         </>
